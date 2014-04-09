64ba2ddac2c01fd9766da66b6dc9f66aee4c506c
dfockler <dfockler13@gmail.com>
2014-04-09 16:41:42 -0700

    reloading from the database into the backend form

--- fieldtypes/RecurringDate_AdvancedDateFieldType.php
+++ fieldtypes/RecurringDate_AdvancedDateFieldType.php
@@ -20,12 +20,111 @@ class RecurringDate_AdvancedDateFieldType extends BaseFieldType
 	public function getInputHtml($name, $value)
 	{
 
+		$fieldValues = array(
+			'name' => $name,
+			'value' => $value,
+		);
+
+		//If it repeats and has an rrule object
+		if( strpos($value, "FREQ") !== false ){
+			$rule = new Recurr\RecurrenceRule($value);
+
+			$startDateStr = $rule->getStartDate()->format('Ymd');
+			$startDate = \Craft\DateTime::createFromFormat('Ymd', $startDateStr);
+			
+			//If a time is actually set on the start date
+			if( strpos( explode(';', explode( 'DTSTART=', $value)[1])[0], 'T') ) {
+				$startTimeStr = $rule->getStartDate()->format('His');
+				$startTime = \Craft\DateTime::createFromFormat('His', $startTimeStr);
+			}
+			else{
+				$startTime = null;
+			}
+
+			if( strpos($value, "BYMONTHDAY") ){
+				$fieldValues['by'] = 'month';
+			}
+			elseif( strpos($value, "BYDAY") ) {
+				$fieldValues['by'] = 'week';
+			}
+
+			$fieldValues['repeats'] = true;
+			$fieldValues['interval'] = strtolower($rule->getFreqAsText());
+			$fieldValues['every'] = $rule->getInterval();
+			$fieldValues['on'] = $rule->getByDay();
+		}
+		else{
+			$startDateTime = rtrim(explode('DTSTART=', $value)[1], ';');
+			$startDate = \Craft\DateTime::createFromFormat('Ymd\THis', $startDateTime);
+
+			if( strpos($startDateTime, 'T') ) {
+				$startTimeStr = date('His', strtotime($startDateTime));
+				$startTime = \Craft\DateTime::createFromFormat('His', $startTimeStr);
+			}
+			else{
+				$startTime = null;
+			}
+
+			$fieldValues['repeats'] = false;
+		}
+
+		$fieldValues['startdate'] = $startDate;
+		$fieldValues['starttime'] = $startTime;
+
+		//If an end date was set
+		if( strpos($value, "DTEND") !== false ){
+			$endDateTime = explode(';', explode( 'DTEND=', $value)[1])[0];
+			$endDate = \Craft\DateTime::createFromFormat('Ymd\THis', $endDateTime);
+			
+			//If an end time was set
+			if( strpos($endDateTime, 'T') ) {
+				$endTimeStr = date('His', strtotime($endDateTime));
+				$endTime = \Craft\DateTime::createFromFormat('His', $endTimeStr);
+			}
+			else{
+				$endTime = null;
+			}
+
+			$fieldValues['enddate'] = $endDate;
+			$fieldValues['endtime'] = $endTime;
+		}
+		else{
+			$fieldValues['enddate'] = null;
+			$fieldValues['endtime'] = null;
+		}
+
+		if( strpos($value, "COUNT") !== false ){
+			$fieldValues['ends'] = 'after';
+			$fieldValues['occurrences'] = $rule->getCount();
+		}
+		elseif( strpos($value, "UNTIL") !== false ){
+			$fieldValues['ends'] = 'until';
+			$untilDateStr = $rule->getUntil()->format('Ymd');
+			$untilDate = \Craft\DateTime::createFromFormat('Ymd', $untilDateStr);
+			$fieldValues['untildate'] = $untilDate;
+		}
+
+		if( strpos($value, "ALLDAY") !== false ){
+			$fieldValues['allday'] = true;
+		}
+		
 		$id = craft()->templates->formatInputId($name);
 
-		return craft()->templates->render('recurringdate/fields', array(
-			'name'  => $name,
-			'value' => $value
-		));
+		return craft()->templates->render('recurringdate/fields', $fieldValues);
+		// $allday = $value['allday'];
+		// $startDate = $value['startdate']['date'];
+		// $startTime = $value['starttime']['time'];
+		// $endDate = $value['enddate']['date'];
+		// $endTime = $value['endtime']['time'];
+		// $repeats = $value['repeats']; //Does it repeat?
+		// $frequency = $value['interval']; //Weekly, Daily, Monthly, Yearly
+		// $interval = $value['every']; // i.e. Every 1-30 Months?
+		// $weekDays = $value['on']; //Which weekdays
+		// $repeatBy = $value['by']; //Monthly, by day of week, or day of month
+		// $repeatStart = $value['startdate']['date']; //May not use this
+		// $ends = $value['ends']; //how it ends (never, after, until)
+		// $count = $value['occurrences']; // if ending occurs amounts
+		// $untilDate = $value['untildate']['date']; // if ending until date
 	}
 
 	public function validate($value){
@@ -79,10 +178,11 @@ class RecurringDate_AdvancedDateFieldType extends BaseFieldType
 		$interval = $value['every']; // i.e. Every 1-30 Months?
 		$weekDays = $value['on']; //Which weekdays
 		$repeatBy = $value['by']; //Monthly, by day of week, or day of month
-		$repeatStart = $value['startdate']['date']; //May not use this
 		$ends = $value['ends']; //how it ends (never, after, until)
 		$count = $value['occurrences']; // if ending occurs amounts
 		$untilDate = $value['untildate']['date']; // if ending until date
+
+		$dbString = '';
 
 
 		//Builds RRULE based on UI Elements Input
@@ -135,22 +235,39 @@ class RecurringDate_AdvancedDateFieldType extends BaseFieldType
 
 			if($startTime){
 				$time = strtotime($startDate . ' ' . $startTime);
-				return 'DTSTART=' . date('Ymd\THis', $time) . ';' . $rule->getString();
+				$dbString .= 'DTSTART=' . date('Ymd\THis', $time) . ';' . $rule->getString();
 			}
 			else{
 				$time = strtotime($startDate);
-				return 'DTSTART=' . date('Ymd', $time) . ';' . $rule->getString();
+				$dbString .= 'DTSTART=' . date('Ymd', $time) . ';' . $rule->getString();
 			}
 		}
 		else{
 			if($startTime){
 				$time = strtotime($startDate . ' ' . $startTime);
-				return 'DTSTART=' . date('Ymd\THis', $time) . ';';
+				$dbString .= 'DTSTART=' . date('Ymd\THis', $time) . ';';
 			}
 			else{
 				$time = strtotime($startDate);
-				return 'DTSTART=' . date('Ymd', $time) . ';';
+				$dbString .= 'DTSTART=' . date('Ymd', $time) . ';';
 			}		
 		}
+
+		if($endDate){
+			if($endTime){
+				$time = strtotime($endDate . ' ' . $endTime);
+				$dbString = 'DTEND=' . date('Ymd\THis', $time) . ';' . $dbString;
+			}
+			else{
+				$time = strtotime($endDate);
+				$dbString .= 'DTEND=' . date('Ymd', $time) . ';' . $dbString;
+			}
+		}
+
+		if($allday){
+			$dbString = 'ALLDAY;' . $dbString;
+		}
+
+		return $dbString;
 	}
 }
